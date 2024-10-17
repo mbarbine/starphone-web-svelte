d8830998b42fcd12ff7e93989f535b20ba34d305
mbarbine <barbine.michael@gmail.com>
2024-10-17 18:50:17 -0400

    update

--- svelte.config.js
+++ svelte.config.js
@@ -2,21 +2,17 @@ import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 const config = {
-  // Preprocess settings for TypeScript, PostCSS, and SCSS
   preprocess: preprocess({
     typescript: true,  // Enable TypeScript support
     postcss: true,     // Enable PostCSS for CSS transformations
     scss: {
-      // If you don't need a global SCSS file, just remove prependData
-      // Or comment this out if you're not using SCSS variables
-      // prependData: `@import 'src/lib/styles/variables.scss';`
+      prependData: `@import 'src/lib/styles/variables.scss';` // Only if you have SCSS variables
     },
     sourceMap: true,   // Enable source maps for easier debugging
   }),
 
   kit: {
     adapter: adapter(),
-
     alias: {
       $lib: 'src/lib',
       $components: 'src/lib/components',
@@ -25,11 +21,10 @@ const config = {
       $videos: 'static/videos',
       $utils: 'src/lib/utils',
     },
-
     prerender: {
-      entries: ['*'], // Prerender all routes by default
-      concurrency: 5, // Limit concurrency for better performance on large builds
-    }
+      entries: ['*'],
+      concurrency: 5,
+    },
   }
 };
 
