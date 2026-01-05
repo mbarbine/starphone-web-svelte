31f9ea350dae4cfee6c1c4f4b6e1891cfd6838ab
Michael Barbine <barbine.michael@gmail.com>
2026-01-04 21:05:05 -0500

    d

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
   
   images: {
     formats: ['image/avif', 'image/webp'],
